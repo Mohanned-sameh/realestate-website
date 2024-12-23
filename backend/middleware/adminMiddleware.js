c490febcdff6dff6a2368e6d706ccc7adf6daecb
Mohanned-sameh <mohanned2678@gmail.com>
2024-12-23 18:57:00 +0200

    Remove unused backend and frontend files, including environment variables, models, routes, and configuration for a cleaner codebase.

--- backend/middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-  if (req.user.accountType !== 'admin' || req.user.accountType !== 'agent') {
-    return res.status(403).json({ message: 'Access denied' });
-  }
-  next();
-};
-
-module.exports = adminMiddleware;
--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className='container'>
-      <h1>React App</h1>
-    </div>
-  );
-}
-
-export default App;
